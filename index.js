b581a7af17e285f44a9fd6870bfa77be89b3da72
Randa_Mike <michaelranda95@gmail.com>
2024-04-15 12:16:07 +0300

    committed a fetch request

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+//  async function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+  
+//   return fetch("https://anapioficeandfire.com/api/books", {
+//     method: 'GET',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     }
+//   })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => { 
+//     console.log(data);
+//     return data;
+//   })
+//   .catch((error) => console.error(error));
+// }
+  
+  
+
+  
+
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+   return fetch("https://anapioficeandfire.com/api/books")
+          .then(response => response.json())
+          .then(books => renderBooks(books));           
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
